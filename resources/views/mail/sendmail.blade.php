4c5a79c349c3f15e94e3d92d985fac3f32767d7e
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-12-13 16:02:23 +0100

    Added comment

--- resources/views/mail/sendmail.blade.php
+++ resources/views/mail/sendmail.blade.php
@@ -1,6 +1,5 @@
 <!-- The only way to do great work is to love what you do. - Steve Jobs -->
 
 {{-- Qui testo da visualizzare in mail che arriva --}}
-
 Hai ricevuto una comunicazione dal sito
 E' stata mandata da {{ $NomeMittente }} {{ $CognomeMittente }} {{ $EmailMittente }} {{ $TestoMessaggioMittente }}
