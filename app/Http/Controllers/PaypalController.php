d200e2fc896846aad59484a5244e9448bd059bb9
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-12-30 13:30:10 +0100

    Integrating paypal payment

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -5,10 +5,32 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Import the class namespaces first, before using it directly
+use Srmklive\PayPal\Services\PayPal as PayPalClient;
+
 class PaypalController extends Controller
 {
-    public function paypal(Request $request){
-
+    public function paypal(Request $request)
+    {
+        $provider = new PayPalClient;
+        $provider->setApiCredentials(config('paypal'));
+        $paypalToken = $provider->getAccessToken();
+        $response = $provider->createOrder([
+            "intent" => "CAPTURE",
+            "application_context" => [
+                "return_url" => route('success'),
+                "cancel_url" => route('cancel'),
+            ],
+            "purchase_units" => [
+                [
+                    "amount" => [
+                        "currency_code" => "EUR",
+                        "value" => $request->price
+                    ]
+                ]
+            ]
+        ]);
+        dd($response);
     }
 
     public function success(){
