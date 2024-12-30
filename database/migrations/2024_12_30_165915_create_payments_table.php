447c6c6aee45c62b9beb8416b33f7bb0b70260f2
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-12-30 18:22:36 +0100

    Paypal database created and migrated

--- database/migrations/2024_12_30_165915_create_payments_table.php
+++ database/migrations/2024_12_30_165915_create_payments_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->string('payment_id');
+            $table->string('payer_name');
+            $table->string('payer_surname');
+            $table->string('payer_email');
+            $table->string('payer_address');
+            $table->string('amount');
+            $table->string('currency');
+            $table->string('payment_method');
+            $table->string('payment_status');
             $table->timestamps();
         });
     }
