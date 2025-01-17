4dc174c48e881d6e0973944f6409590097a95395
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2025-01-17 19:23:20 +0100

    Old settings

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class ContentSecurityPolicy
 {
@@ -14,27 +15,39 @@ class ContentSecurityPolicy
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+
     public function handle(Request $request, Closure $next): Response
     {
-        $nonce = Str::random(32);
-        $request->attributes->set('csp-nonce', $nonce);
+        Log::stack(['single'])->emergency('CSP Middleware START');
 
+        // $nonce = Str::random(32);
+        // $request->attributes->set('csp-nonce', $nonce);
+        
         $response = $next($request);
 
-        $cspHeader = "default-src 'self'; " .
-            "script-src 'self' 'nonce-{$nonce}' https://cdnjs.cloudflare.com; " .
-            "style-src 'self' 'nonce-{$nonce}' https://fonts.googleapis.com; " .
-            "img-src 'self' data: blob:; " .
-            "font-src 'self' https://fonts.gstatic.com; " .
-            "connect-src 'self'; " .
-            "media-src 'self'; " .
-            "object-src 'none'; " .
-            "base-uri 'self'; " .
-            "form-action 'self'; " .
-            "frame-ancestors 'none'; " .
-            "upgrade-insecure-requests;";
+        try {
+            Log::stack(['single'])->emergency('Setting CSP headers');
+            
+            $cspHeader = "default-src 'self'; " .
+                "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com; " .
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " .
+                "img-src 'self' data: blob:; " .
+                "font-src 'self' https://fonts.gstatic.com; " .
+                "connect-src 'self' ws: wss:; " .
+                "media-src 'self'; " .
+                "object-src 'none'; " .
+                "base-uri 'self'; " .
+                "form-action 'self'; " .
+                "frame-ancestors 'none'";
 
-        $response->headers->set('Content-Security-Policy', $cspHeader);
+            $response->headers->set('Content-Security-Policy', $cspHeader);
+            Log::stack(['single'])->emergency('CSP header set: ' . $cspHeader);
+
+        } catch (\Exception $e) {
+            Log::stack(['single'])->emergency('Error in CSP middleware: ' . $e->getMessage());
+        }
+
+        Log::stack(['single'])->emergency('CSP Middleware END');
 
         return $response;
     }
---
+++ config/security.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'csp' => [
+        'enabled' => env('CSP_ENABLED', true),
+        'report_only' => env('CSP_REPORT_ONLY', false),
+        'report_uri' => env('CSP_REPORT_URI', null),
+    ],
+];
